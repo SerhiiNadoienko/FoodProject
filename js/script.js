36cd0b7ca949cd3171ded89ee324fb1eea555d53
Serhii <sergenadoenko@gmail.com>
2022-09-05 10:07:15 +0200

    used a new technology(fetch) with JSON

--- js/script.js
+++ js/script.js
@@ -233,29 +233,31 @@ window.addEventListener('DOMContentLoaded', function() {
                 margin: 0 auto;
             `;
             form.insertAdjacentElement('afterend', statusMessage);
-        
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
-            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+           
             const formData = new FormData(form);
 
             const object = {};
             formData.forEach(function(value, key){
                 object[key] = value;
             });
-            const json = JSON.stringify(object);
-
-            request.send(json);
-
-            request.addEventListener('load', () => {
-                if (request.status === 200) {
-                    console.log(request.response);
-                    showThanksModal(message.success);
-                    statusMessage.remove();
-                    form.reset();
-                } else {
-                    showThanksModal(message.failure);
-                }
+            fetch('server1.php', {
+                method:'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body:JSON.stringify(object)
+            })
+            .then(data => data.text())
+            .then(data=> {
+                //data это данные из промимса(которые вернул сервер)
+                console.log(data);
+                showThanksModal(message.success);
+                statusMessage.remove();
+                form.reset();
+            }).catch(()=> {
+                showThanksModal(message.failure);
+            }).finally(()=> {
+                form.reset();
             });
         });
     }
@@ -282,4 +284,5 @@ window.addEventListener('DOMContentLoaded', function() {
             closeModal();
         }, 4000);
     }
+
 });
